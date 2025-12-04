d164006d5d1ed7b64c15cb2cb4a563f421892939
mjrutala <mrutala@bu.edu>
2025-12-04 15:01:05 -0500

    Standardized code; streamlined JPL Horizons call to avoid needing CXO orbit file and associated math; preserved command line interface

--- python_sso_freeze_v6.py
+++ python_sso_freeze_v6.py
@@ -60,122 +60,141 @@
 import numpy as np
 import pandas as pd
 import os
-from astropy.io import fits as pyfits
+# from astropy.io import fits as pyfits
+import astropy
+from astropy.time import Time                   #convert between different time coordinates
+from astropy.time import TimeDelta              #add/subtract time intervals 
+from astropy import units as u
 from scipy import interpolate
+import datetime
+
+# import the defined functions to analysis Chandra data nad perfrom coordinate transformations
+import go_chandra_analysis_tools as gca_tools
 
 """Reading in the config.ini file containing any hard wired inputs """
 import configparser
 
-def sso_freeze():
+def find_event_filepath(acis, obs_id, obs_dir, suffix='evt2.fits'):
+    """
+    Spun off from sso_freeze so other functions can call this directly
+    Rather than constantly reinventing the wheel
+    """
+    event_filepath = []
     
-    
-    config = configparser.ConfigParser()
-    config.read('config.ini')
-    
-    
-    """Below are a list of defined functions used in the code below
-    ----------------------------------------------------------------"""
-    
-    
-    def doy_frac(DOY, hour, minutes, seconds): # function takes a given DOY, hour, minutes, seconds...
-        frac = DOY + hour/24.0 + minutes/1440.0 + seconds/86400.0 #... and calualtes the correct DOY fraction.
-        return frac
-    
-    # AU to meter conversion - useful later on (probably a function built in already)
-    AU_2_m = 1.49598E+11
-    
-    ACIS = str(config['inputs']['ACIS'])
-    
-    """SECTION 1)"""
-    
-    folder_path = str(config['inputs']['folder_path'])
-    
-    # The code below reads in the Chandra event file and extracts the relevant header info needed later
-    evt_location = []
-    
-    if ACIS == 'y':
-        for file in os.listdir(str(folder_path)):
-            if file.startswith("acisf") and file.endswith("evt2.fits"):
-                evt_location.append(os.path.join(str(folder_path), file))
-    
+    if acis == 'y':
+        for file in os.listdir(str(obs_dir)):
+            if file.startswith("acisf") and file.endswith(suffix):
+                event_filepath.append(os.path.join(str(obs_dir), file))
     else:
-        for file in os.listdir(str(folder_path)):
-            if file.startswith("hrcf") and file.endswith("evt2.fits"):
-                evt_location.append(os.path.join(str(folder_path), file))
+        for file in os.listdir(str(obs_dir)):
+            if file.startswith("hrcf"+obs_id) and file.endswith(suffix):
+                event_filepath.append(os.path.join(str(obs_dir), file))
+                
+    event_filepath = event_filepath[0]
+    return event_filepath
     
+def find_orbit_filepath(acis, obs_id, obs_dir):
+    """
+    Spun off from sso_freeze so other functions can call this directly
+    Rather than constantly reinventing the wheel
+    """
+    orbit_filepath = []
     
-    evt_file = pyfits.open(evt_location[0])
-    # event file is read in and opened to extract header info
-    evt_hdr = evt_file[1].header # event file header...
-    evt_data = evt_file[1].data #...and associated date with each header
-    evt_time = evt_data['TIME'] # time of each photon observred as it reaches detector(???)
-    tstart = evt_hdr['TSTART'] # start time of observation (in seconds)
-    tend = evt_hdr['TSTOP'] # end time of observation (in seconds)
-    obs_id = evt_hdr['OBS_ID']
-    start_date = evt_hdr['DATE-OBS'] # start date of observation
-    evt_x = evt_data['X'] # x position of target
-    evt_y = evt_data['Y'] # y position of target
-    
-    #end_time = evt_hdr['TSTOP']
-    RA_0 = evt_hdr['RA_NOM'] # RA origin of target at the start of the observation
-    DEC_0 = evt_hdr['DEC_NOM'] # DEC origin of target at the start of the observation
-    evt_file.close()
-    
-    evt_date = pd.to_datetime(start_date) # converts the date to a timestamp - to allow the time to be separated and used for
-    # calculation of the DOY and DOYFRAC
-    evt_hour = evt_date.hour # hour of observation
-    evt_doy = evt_date.strftime('%j') # doy of observation
-    evt_mins = evt_date.minute # minute of observation
-    evt_secs = evt_date.second # second of observation
-    evt_DOYFRAC = doy_frac(float(evt_doy), float(evt_hour), float(evt_mins), float(evt_secs)) # calculating the DOYFRAC
-    chand_time = (evt_time - tstart)/86400.0 # calculating time cadence of chandra...
-    doy_chandra = chand_time + evt_DOYFRAC #... to calculate the DOY of chandra
-    
-    
-    """SECTION 2)"""
-    # The code below reads in the orbit empheris file for the identified OBSid
-    
-    orb_location = []
-    
-    for file in os.listdir(str(folder_path)):
+    for file in os.listdir(str(obs_dir)):
         if file.startswith("orbit") and file.endswith("eph1.fits") or file.endswith("eph0.fits"):
-            orb_location.append(os.path.join(str(folder_path), file))
+            orbit_filepath.append(os.path.join(str(obs_dir), file))
+                
+    orbit_filepath = orbit_filepath[0]
+    return orbit_filepath
+
+def sso_freeze(config=None, acis=None, obs_id=None, obs_dir=None):
+    breakpoint()
     
-    # file used corresponding to the event file
+    # If acis, obs_id, and obs_dir are specified, they take precedence
+    if (acis is not None) & (obs_id is not None) & (obs_dir is not None):
+        pass
+    else:
+        # Parse config file
+        cfg = configparser.ConfigParser()
+        cfg.read(config)
+        
+        # ACIS = str(config['inputs']['ACIS'])
+        # obs_id = config['inputs']['obsID']
+        # folder_path = str(config['inputs']['folder_path'])
+        acis = str(config['inputs']['ACIS'])
+        obs_id = config['inputs']['obsID']
+        obs_dir = str(config['inputs']['folder_path'])
     
-    orb_file = pyfits.open(orb_location[0])
-    # orbit empheris file is read in...
-    hdr = orb_file[1].header #...header information is extacted...
-    data = orb_file[1].data #...and the relevant data us also extracted
-    orb_time = data['TIME'] # time of observation when Jovian photons reach spaecraft 
-    orb_x = data['X'] # x position of spacecraft
-    orb_y = data['Y'] # y position of spacecraft
-    orb_z = data['Z'] # z position of spacecraft
-    orb_file.close()
+    # =============================================================================
+    # Reading in the Chandra events file (uncorrected) and extract relevant header info
+    # =============================================================================
     
-    doy_sc = (orb_time - tstart) /86400.0 + evt_DOYFRAC # doy of spacecraft
+    # # The code below reads in the Chandra event file and extracts the relevant header info needed later
+    # evt_location = []
+    # if ACIS == 'y':
+    #     for file in os.listdir(str(folder_path)):
+    #         if file.startswith("acisf") and file.endswith("evt2.fits"):
+    #             evt_location.append(os.path.join(str(folder_path), file))
+    # else:
+    #     for file in os.listdir(str(folder_path)):
+    #         if file.startswith("hrcf") and file.endswith("evt2.fits"):
+    #             evt_location.append(os.path.join(str(folder_path), file))
     
-    """SECTION 3)"""
+    event_filepath = find_event_filepath(acis, obs_id, obs_dir)
     
+    # Read the event file and access data and header with context manager
+    with astropy.io.fits.open(event_filepath) as hdul:
+        event_data = hdul[1].data 
+        event_hdr = hdul[1].header
+        
+        
+    event_time = event_data['TIME'] # Spaceraft time of photon event
+    event_x = event_data['X'] # Sky X coordinate of photon event
+    event_y = event_data['Y'] # Sky Y coordinate of photon event
     
-    """Brad's horizons code to extract the ephemeris file"""
+    tstart = event_hdr['TSTART'] # Start time of obs. in s/c time
+    tstop = event_hdr['TSTOP'] # Stop time of obs. in s/c time
+    # obs_id = event_hdr['OBS_ID']
+    start_date = event_hdr['DATE-OBS'] # Start time of obs. as str
+    RA_0 = event_hdr['RA_NOM'] # RA origin of target at the start of the observation
+    DEC_0 = event_hdr['DEC_NOM'] # DEC origin of target at the start of the observation
     
-    from astropy.time import Time                   #convert between different time coordinates
-    from astropy.time import TimeDelta              #add/subtract time intervals 
-    from astroquery.jplhorizons import Horizons     #automatically download ephemeris 
+    event_date = pd.to_datetime(start_date) # converts the date to a timestamp - to allow the time to be separated and used for
+    # calculation of the DOY and DOYFRAC
+    event_hour = event_date.hour # hour of observation
+    event_doy = event_date.strftime('%j') # doy of observation
+    event_mins = event_date.minute # minute of observation
+    event_secs = event_date.second # second of observation
+    event_DOYFRAC = gca_tools.doy_frac(float(event_doy), float(event_hour), float(event_mins), float(event_secs)) # calculating the DOYFRAC
+    chand_time = (event_time - tstart)/86400.0 # calculating time cadence of chandra...
+    doy_chandra = chand_time + event_DOYFRAC #... to calculate the DOY of chandra
     
-    # The start and end times are taken from the horizons file.
-    tstart_eph=Time(tstart, format='cxcsec')
+    # =============================================================================
+    # Reading in the orbit empheris file and extract relevant header info 
+    # All of this is done with ephemerides from Horizons now
+    # =============================================================================
     
-    dt = TimeDelta(0.125, format='jd')
-    tstop_eph=Time(tend, format='cxcsec')
+    # orbit_filepath = find_orbit_filepath(ACIS, obs_id, folder_path)
+    # with astropy.io.fits.open(orbit_filepath) as hdul:
+    #     # orbit empheris file is read in...
+    #     # hdr = orb_file[1].header #...header information is extacted...
+    #     # data = orb_file[1].data #...and the relevant data us also extracted
+    #     orb_time = hdul[1].data['TIME'] # time of observation when Jovian photons reach spaecraft 
+    #     orb_x = hdul[1].data['X'] # x position of spacecraft
+    #     orb_y = hdul[1].data['Y'] # y position of spacecraft
+    #     orb_z = hdul[1].data['Z'] # z position of spacecraft
+    # doy_sc = (orb_time - tstart) /86400.0 + event_DOYFRAC # doy of spacecraft
     
-    # Below sets the parameters of what observer (geocentric) the ephemeris file is generated form. For example, '500' = centre of the Earth, '500@-151' = CXO
-    obj = Horizons(id=599,location='500',epochs={'start':(tstart_eph).iso, 'stop':(tstop_eph+dt).iso, 'step':'5m'}, id_type='majorbody')
-    eph_jup = obj.ephemerides()
+    # =============================================================================
+    # Read in the ''chandra_horizons file'', extract relevant info and interpolate the data
+    # =============================================================================
+    
+    start_time = Time(tstart, format='cxcsec')
+    stop_time = Time(tstop, format='cxcsec')
+    delta_time = '5m'
+    eph_jup = gca_tools.fetch_ephemerides_fromCXO(start_time, stop_time, delta_time)
     
     # Extracts relevent date/time information needed from ephermeris file
-    
     eph_dates = pd.to_datetime(eph_jup['datetime_str'])
     eph_dates = pd.DatetimeIndex(eph_dates)
     eph_doy = np.array(eph_dates.strftime('%j')).astype(int)
@@ -183,32 +202,37 @@ def sso_freeze():
     eph_minutes = eph_dates.minute
     eph_seconds = eph_dates.second
     
-    eph_doyfrac = doy_frac(eph_doy, eph_hours, eph_minutes, eph_seconds) # DOY fraction from ephermeris data
+    eph_doyfrac = gca_tools.doy_frac(eph_doy, eph_hours, eph_minutes, eph_seconds) # DOY fraction from ephermeris data
     
-    """SECTION 4)"""
+    # =============================================================================
+    # Apply the correction to allow the photons to be tracked to their position on Jupiter
+    # =============================================================================
     
-    interpfunc_x = interpolate.interp1d(doy_sc, orb_x,fill_value="extrapolate")
-    interpfunc_y = interpolate.interp1d(doy_sc, orb_y,fill_value="extrapolate")
-    interpfunc_z = interpolate.interp1d(doy_sc, orb_z,fill_value="extrapolate")
+    # Again, all below has been replaced with an improved Horizons ephemerides call
+    # Get the orbital (x, y, z) at the times of ephemerides
+    # interpfunc_x = interpolate.interp1d(doy_sc, orb_x, fill_value="extrapolate")
+    # interpfunc_y = interpolate.interp1d(doy_sc, orb_y, fill_value="extrapolate")
+    # interpfunc_z = interpolate.interp1d(doy_sc, orb_z, fill_value="extrapolate")
     
-    # Above code creates a linear intepolation function that interpolates the spacecraft DOY and the positional coordinates from
-    # the orbital empheris file to...
+    # orb_x_interp = interpfunc_x(eph_doyfrac)
+    # orb_y_interp = interpfunc_y(eph_doyfrac)
+    # orb_z_interp = interpfunc_z(eph_doyfrac)
     
-    orb_x_interp = interpfunc_x(eph_doyfrac)
-    orb_y_interp = interpfunc_y(eph_doyfrac)
-    orb_z_interp = interpfunc_z(eph_doyfrac)
+    # breakpoint()
     
-    #... the DOY from the empheris file (2 day window in this case) to produce the new orbit positional coordinates within this time
-    #range
-    r_jup = np.array(eph_jup['delta'].astype(float))*AU_2_m # Jupiter chandra distance 
-    dec_jup = np.deg2rad(np.array(eph_jup['DEC'].astype(float))) # DEC of Jupiter during observation
-    ra_jup = np.deg2rad(np.array(eph_jup['RA'].astype(float)))# RA of Jupiter duting observation
-    xp = (r_jup * np.cos(dec_jup) * np.cos(ra_jup)) - orb_x_interp
-    yp = (r_jup * np.cos(dec_jup) * np.sin(ra_jup)) - orb_y_interp
-    zp = (r_jup * np.sin(dec_jup)) - orb_z_interp
-    # Above is (x,y,z) position of Jupiter during obsrvation and...
+    # Jupiter-Chandra distance, in meters
+    r_jup = np.array(eph_jup['delta'].astype(float))*(u.au.to(u.m))  
+    # DEC of Jupiter during observation, in rad
+    dec_jup = np.deg2rad(np.array(eph_jup['DEC'].astype(float))) 
+    # RA of Jupiter duting observation, in rad
+    ra_jup = np.deg2rad(np.array(eph_jup['RA'].astype(float)))
+    
+    # (xp, yp, zp) are the components of CXO-Jupiter distance
+    xp = (r_jup * np.cos(dec_jup) * np.cos(ra_jup))
+    yp = (r_jup * np.cos(dec_jup) * np.sin(ra_jup))
+    zp = (r_jup * np.sin(dec_jup))
+    
     rp = np.sqrt(xp**2 + yp**2 + zp**2)
-    #... the absoulte value of the Jupiter_Earth radius vector
     rap_jup = (np.rad2deg(np.arctan2(yp,xp)) + 720.0) % 360  # RA of Jupiter at the observed coordinates
     decp_jup = (np.rad2deg(np.arcsin(zp/rp))) # DEC of Jupiter at the observed coordinates
     
@@ -220,33 +244,50 @@ def sso_freeze():
     dec_jup_interp = interpfunc_dec_jup(doy_chandra) # interpolated DEC of Jupiter and the DOY from the emphermis file to the Chandra
     #DOY
     
-    if ACIS == 'y':
+    if acis == 'y':
         scale = 0.4920 # units of pixels/arcsec
-        xx = (evt_x - (RA_0 - ra_jup_interp) * 3600.0 / scale * cc).astype(float) # corrected x position of photons
-        yy = (evt_y + (DEC_0 - dec_jup_interp) * 3600.0 / scale).astype(float) # corrected y position of photons
+        xx = (event_x - (RA_0 - ra_jup_interp) * 3600.0 / scale * cc).astype(float) # corrected x position of photons
+        yy = (event_y + (DEC_0 - dec_jup_interp) * 3600.0 / scale).astype(float) # corrected y position of photons
     
     else:
         scale = 0.13175 # untis of pixels/arcsec 
-        xx = (evt_x - (RA_0 - ra_jup_interp) * 3600.0 / scale * cc).astype(float) # corrected x position of photons
-        yy = (evt_y + (DEC_0 - dec_jup_interp) * 3600.0 / scale).astype(float) # corrected y position of photons
+        xx = (event_x - (RA_0 - ra_jup_interp) * 3600.0 / scale * cc).astype(float) # corrected x position of photons
+        yy = (event_y + (DEC_0 - dec_jup_interp) * 3600.0 / scale).astype(float) # corrected y position of photons
     
     
-    """SECTION 5)"""
-    if ACIS == 'y':
-        new_evt_location = (str(folder_path) + f"/acisf{obs_id}_pytest_evt2.fits") # path of the location
+    # =========================================================================
+    # The new positions of the photons replace the uncorrected positions from
+    # the fits file and is written to a new fits file
+    # =========================================================================
+    if acis == 'y':
+        new_event_filepath = (str(obs_dir) + f"/acisf{obs_id}_ssofreeze_evt2.fits") # path of the location
         # for the corrected fits file (with the photons corrected for the position).
     else:
-        new_evt_location = (str(folder_path) + f"/hrcf{obs_id}_pytest_evt2.fits") # path of the location
+        new_event_filepath = (str(obs_dir) + f"/hrcf{obs_id}_ssofreeze_evt2.fits") # path of the location
         # new_evt_location = f"/Users/daleweigt/Documents/DIAS_2022_2023/Chandra_code/{obs_id}test_v3.fits"
         # for the corrected fits file (with the photons corrected for the position)
     
-    new_evt_data, new_evt_header = pyfits.getdata(evt_location[0], header=True)
-    # original fits file is read in again to obtain the data - data and header values are assigned to a new variable to avoid overwriting
-    # the original file
-    new_evt_data['X'] = xx # New x coord of photons added to fits file under previous data header 'X'
-    new_evt_data['Y'] = yy # New y coord of photons added to fits file under previous data header 'Y'
-    pyfits.writeto(new_evt_location, new_evt_data, new_evt_header, overwrite=True)
-    # new fits file is written with the correction to the position of the photons witht he original file remaining the same
-    #pyfits.close()
     
+    # No context manager here, as we need to edit hdul directly
+    hdul = astropy.io.fits.open(event_filepath)
     
+    hdul[1].data['X'] = xx
+    hdul[1].data['Y'] = yy
+    
+    now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
+    hdul[0].header['COMMENT'] = "Processed by sso_freeze() on {}".format(now)
+    
+    hdul.writeto(new_event_filepath, overwrite=True)
+    hdul.close()
+    
+    return
+    
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--config', help='Config file name.')
+    
+    args = parser.parse_args()
+    config = 'config.ini' if args.config is None else args.config
+    
+    _ = sso_freeze(config = config)
